d3d56003faf032500894517d056b5711b026bf19
Adam Farmer <adam.farmer@just-eat.com>
2016-06-10 16:03:24 +0100

    Added proof project

---
+++ QordobaProof/QordobaProof/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QordobaProof.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index(string culture)
+        {
+            if (!string.IsNullOrEmpty(culture))
+            {
+                Thread.CurrentThread.CurrentCulture = new CultureInfo(culture);
+                Thread.CurrentThread.CurrentUICulture = new CultureInfo(culture);
+            }
+            return View();
+        }
+        
+    }
+}
